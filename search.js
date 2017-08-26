467a3996d0d61a3608d8f42688c09fedcd1509da
Needs-A-TARDIS <eeyi.oon@gmail.com>
2017-08-25 23:41:20 -0400

    API First Test
    Switching from jquery to regular XMLHttpRequest
    Changes to index.html to test whether getting stuff from the API works

--- search.js
+++ search.js
@@ -1,13 +1,23 @@
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://api.themoviedb.org/3/search/keyword?page=1&query=tomorrowland&api_key=c86ce297941a783af9f40b9a9985424d",
-  "method": "GET",
-  "headers": {},
-  "data": "{}"
-}
+function mysearch() {
+//api query & settings 
+var data = "{}";
+var query = "wonder";
+var apikey = "c86ce297941a783af9f40b9a9985424d";
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
 
-$.ajax(settings).done(function (response) {
- console.log(response);
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    console.log(this.responseText);
+  }
 });
-//$(document).ready(
+var url = "https://api.themoviedb.org/3/search/keyword?page=1&query=" +query+"&api_key="+apikey;
+xhr.open("GET", url);
+
+xhr.send(data);
+var result = xhr.responseText; 
+return result;
+}
+document.getElementById("uin").innerHTML = mysearch();
+
+
